aea9a4c568a5c968d7518a22af55bf7277a7460c
Kenethproox <159308249+Kenethproox@users.noreply.github.com>
2024-02-07 11:26:14 -0600

    Add files via upload

---
+++ S4-L1-KDSF_1272924.py
@@ -0,0 +1,18 @@
+'''
+PENSAMIENTO COMPUTACIONAL - SECCIÓN 12
+07/02/2024
+Keneth David Salguero Felipe
+Objetivo: Usar operadores
+
+Entrada: num_1, num_2
+Procesos Principales:  sumar, restar, multiplicar y dividir
+Salida: resultados de las operaciones
+'''
+print("Mi primer programa")
+num_1 = int(input("Ingrese el primer número"))
+num_2 = int(input("Ingrese el segundo número"))
+suma = num_1 + num_2
+resta = num_1 - num_2
+multi = num_2 * num_1
+division = num_1 / num_2
+print("La suma es:{}, la resta es: {}, La multiplicación es: {} y la división es: {}".format(suma, resta, multi, division))
